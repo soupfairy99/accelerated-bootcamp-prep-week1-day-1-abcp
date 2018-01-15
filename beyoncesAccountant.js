5f5f16b9677a228db796f6ad31e23d714a963101
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-15 05:06:32 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -1,7 +1,7 @@
 var totalSum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000;
 console.log("Dear Beyonce,")
 console.log("We have calculated your total earnings this year.")
-console.log("You have earned " + totalSum)
+console.log("You have earned " + totalSuml)
 console.log("Of "+  totalSum +" some portion derives from your live performances.")
 console.log("Of " + totalSum + " some portion derives from music sales.")
 
